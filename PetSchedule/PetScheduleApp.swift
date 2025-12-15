317532283c985dd59a95204c97012d88d8dacf0f
RustOytov <rustam.oytov22@gmail.com>
2025-12-16 00:04:21 +0300

    Initial commit

---
+++ PetSchedule/PetScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetScheduleApp.swift
+//  PetSchedule
+//
+//  Created by Polaroytov on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct PetScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
